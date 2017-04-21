16c503b58c18b27619676ab70ec67e6eb99ccdf8
Akshay Tandon <atandon0@users.noreply.github.com>
2017-04-21 13:23:37 -0500

    Part II complete (excl. format)

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -12,8 +12,6 @@ class CalculationsController < ApplicationController
   end
 
   def square_form
-    @user_number = params["num"].to_f
-    @square_root = Math.sqrt(@user_number)
     render("calculations/square_form.html.erb")
   end
 
@@ -22,6 +20,17 @@ class CalculationsController < ApplicationController
     @square = @user_number ** 2
     render("calculations/square.html.erb")
   end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @user_number = params[:user_number].to_f
+    @square_root = (Math.sqrt(@user_number)).round(4)
+    render("calculations/square_root.html.erb")
+  end
+
 
   def flexible_pmt
     @basis_points = params["basis_points"].to_f
@@ -35,5 +44,37 @@ class CalculationsController < ApplicationController
     render("calculations/flexible_pmt.html.erb")
   end
 
+  def flexible_random
+    @min = params["min"].to_i
+    @max = params["max"].to_i
+    @random_number = rand(@min..@max)
+    render("calculations/flexible_random.html.erb")
+  end
+
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
+
+  def payment
+    @percent = params[:apr].to_f
+    @percent_decimal_months = (@percent/100)/12
+    @number_of_years = (params[:years].to_f).round()
+    @periods = @number_of_years*12
+    @present_value = (params[:principal].to_f).round(2)
+    @discount_factor = (((1+@percent_decimal_months)**@periods)-1)/(@percent_decimal_months*(1+@percent_decimal_months)**@periods)
+    @monthly_pmt = (@present_value/@discount_factor).round(2)
+    render("calculations/payment.html.erb")
+  end
+
+  def random_form
+    render("calculations/random_form.html.erb")
+  end
+
+  def random
+    @min = params[:min].to_f
+    @max = params[:max].to_f
+    @random_number = (rand(@min..@max)).round(4)
+    render("calculations/random.html.erb")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get("/square/new/", {:controller => "calculations", :action => "square_form"})
   get("/square/results/", {:controller => "calculations", :action => "square"})
   get("/flexible/payment/:basis_points/:number_of_years/:present_value", {:controller => "calculations", :action => "flexible_pmt"})
-
-
-
+  get("/flexible/random/:min/:max", {:controller => "calculations", :action => "flexible_random"})
+  get("/square_root/new/", {:controller => "calculations", :action => "square_root_form"})
+  get("/square_root/results/", {:controller => "calculations", :action => "square_root"})
+    get("/payment/new/", {:controller => "calculations", :action => "payment_form"})
+    get("/payment/results/", {:controller => "calculations", :action => "payment"})
+    get("/random/new/", {:controller => "calculations", :action => "random_form"})
+get("/random/results/", {:controller => "calculations", :action => "random"})
 end
