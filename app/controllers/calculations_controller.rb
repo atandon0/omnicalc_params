2297593328280e8e7187a3cc4fe4ff8a81b29eb5
Akshay Tandon <atandon0@users.noreply.github.com>
2017-04-21 13:56:16 -0500

    Included Word Count

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -77,4 +77,28 @@ class CalculationsController < ApplicationController
     render("calculations/random.html.erb")
   end
 
+  def word_count_form
+    render("calculations/word_count_form.html.erb")
+  end
+
+  def word_count
+    @text = params[:user_text].to_s
+    @special_word = params[:user_word].to_s
+
+    text_downcase = @text.downcase
+    text_split_into_array = text_downcase.split
+    @word_count = text_split_into_array.count
+
+    text_chomp = @text.chomp
+    @character_count_with_spaces = text_chomp.length
+
+    text_no_whiteSpace = text_chomp.gsub(/\s+/, "")
+    @character_count_without_spaces = text_no_whiteSpace.length
+
+    special_word_downcase = @special_word.downcase
+    @occurrences = text_downcase.scan(special_word_downcase).length
+
+    render("calculations/word_count.html.erb")
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
-
   get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root"})
   get("/square/new/", {:controller => "calculations", :action => "square_form"})
   get("/square/results/", {:controller => "calculations", :action => "square"})
@@ -10,8 +9,14 @@ Rails.application.routes.draw do
   get("/flexible/random/:min/:max", {:controller => "calculations", :action => "flexible_random"})
   get("/square_root/new/", {:controller => "calculations", :action => "square_root_form"})
   get("/square_root/results/", {:controller => "calculations", :action => "square_root"})
-    get("/payment/new/", {:controller => "calculations", :action => "payment_form"})
-    get("/payment/results/", {:controller => "calculations", :action => "payment"})
-    get("/random/new/", {:controller => "calculations", :action => "random_form"})
-get("/random/results/", {:controller => "calculations", :action => "random"})
+  get("/payment/new/", {:controller => "calculations", :action => "payment_form"})
+  get("/payment/results/", {:controller => "calculations", :action => "payment"})
+  get("/random/new/", {:controller => "calculations", :action => "random_form"})
+  get("/random/results/", {:controller => "calculations", :action => "random"})
+
+  get("/word_count/new/", {:controller => "calculations", :action => "word_count_form"})
+  get("/word_count/results/", {:controller => "calculations", :action => "word_count"})
+
+  
+
 end
