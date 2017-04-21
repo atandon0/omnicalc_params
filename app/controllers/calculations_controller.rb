a24592207a3c2e86e2f8598e7eda3f139a7b3ffe
Akshay Tandon <atandon0@users.noreply.github.com>
2017-04-21 12:18:52 -0500

    Everything up to flexible pmt

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,39 @@
+class CalculationsController < ApplicationController
+  def flex_square_5
+    @user_number = params["num"].to_f
+    @square = @user_number ** 2
+    render("calculations/flex_square_5.html.erb")
+  end
+
+  def flex_square_root
+    @user_number = params["num"].to_f
+    @square_root = Math.sqrt(@user_number)
+    render("calculations/flex_square_root.html.erb")
+  end
+
+  def square_form
+    @user_number = params["num"].to_f
+    @square_root = Math.sqrt(@user_number)
+    render("calculations/square_form.html.erb")
+  end
+
+  def square
+    @user_number = params[:user_number].to_f
+    @square = @user_number ** 2
+    render("calculations/square.html.erb")
+  end
+
+  def flexible_pmt
+    @basis_points = params["basis_points"].to_f
+    @percent = @basis_points/100
+    @percent_decimal_months = (@percent/100)/12
+    @number_of_years = (params["number_of_years"].to_f).round()
+    @periods = @number_of_years*12
+    @present_value = (params["present_value"].to_f).round(2)
+    @discount_factor = (((1+@percent_decimal_months)**@periods)-1)/(@percent_decimal_months*(1+@percent_decimal_months)**@periods)
+    @monthly_pmt = (@present_value/@discount_factor).round(2)
+    render("calculations/flexible_pmt.html.erb")
+  end
+
+
+end
